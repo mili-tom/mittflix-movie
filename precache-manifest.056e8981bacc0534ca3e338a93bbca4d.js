d74bf78b9cecae8629a66941ac3166009456f6b8
Milijana Tomic <mlgalic@gmail.com>
2020-06-27 04:14:05 -0500

    Updates

---
+++ precache-manifest.056e8981bacc0534ca3e338a93bbca4d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef323dbff40c9cbe0f553469519ee900",
+    "url": "/mittflix-movie/index.html"
+  },
+  {
+    "revision": "198a84624aeaf0ae56fb",
+    "url": "/mittflix-movie/static/css/main.c4a38e6f.chunk.css"
+  },
+  {
+    "revision": "5af153ceb2abe70d4c9f",
+    "url": "/mittflix-movie/static/js/2.1561d92e.chunk.js"
+  },
+  {
+    "revision": "198a84624aeaf0ae56fb",
+    "url": "/mittflix-movie/static/js/main.53ff9992.chunk.js"
+  },
+  {
+    "revision": "19e10706c7a1d264b606",
+    "url": "/mittflix-movie/static/js/runtime~main.cafeac04.js"
+  }
+]);
